0ec7de38c1b8e9ab89edc1c58be7c21e0c3ae812
DranoelMit <tim.matthew.leonard@gmail.com>
2020-08-10 14:07:33 -0400

    stretch progress bar

--- src/index.js
+++ src/index.js
@@ -125,6 +125,7 @@ const VideoPlayer = ({ children, containerClassName, videoId, playButtonColor = 
     } else {
       document.getElementById(`parent-${videoId}`).requestFullscreen()
     }
+    onResize()
   }
 
   const getVolumeIcon = () => {
